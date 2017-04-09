5596b0a7e1dd594c2869b6e78b0c386ab32945cf
beazsiah54 <beazsiah54@gmail.com>
2017-04-09 11:39:25 +0300

    Deleted older version

--- source_YDE_v01.01.02/youtube-dl-updater.bat
+++
@@ -1,8 +0,0 @@
-
-@echo off
-echo Waiting for file handle to be closed ...
-ping 127.0.0.1 -n 5 -w 1000 > NUL
-move /Y "C:\Users\bob\Desktop\source_YDE_v01.01.01\youtube-dl.exe.new" "C:\Users\bob\Desktop\source_YDE_v01.01.01\youtube-dl.exe" > NUL
-echo Updated youtube-dl to version 2017.04.03.
-start /b "" cmd /c del "%~f0"&exit /b"
-                
